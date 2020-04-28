451e2bcd286c648d273603df02e3f941a845ef38
wisdom <hahahihiho16@gmail.com>
2020-04-28 22:02:50 +0900

    [START] index.html + index.js/css + App.js/cs s

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class Subject extends Component{
+  render(){
+    return(
+      <div className="Subject">
+        <header>
+          <h1>{this.props.title}</h1>  
+          {this.props.content}
+        </header>        
+      </div>
+    )
+  }
+}
+
+class List extends Component{
+  render(){
+    return(
+      <ul>
+        <li><a href="1.html">1.html</a></li>
+        <li><a href="2.html">2.html</a></li>
+        <li><a href="3.html">3.html</a></li>
+      </ul>
+    );
+  }
+}
+
+
+class Content extends Component{
+  render(){
+    return(
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.content}
+      </article>
+    )
+  }
+}
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <Subject title="WEB" content="World Wide WEB"/>
+        <Subject title="WEB2" content="World Wide WEB2"/>
+        <List/>
+        <Content title="HTML" content="HTML is HyperText Markup Language"/>
+        <Content title="HTML2" content="HTML2 is HyperText Markup Language"/>
+      </div>
+    );
+  }
 }
 
 export default App;
